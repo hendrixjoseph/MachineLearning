79e05db9c857d4f8f6747a77bab70f13bbc84aa1
hendrixjoseph <hendrixjoseph@aol.com>
2016-10-21 20:09:39 -0400

    first commit

---
+++ src/main/java/edu/wright/hendrix11/DowData.java
@@ -0,0 +1,26 @@
+package edu.wright.hendrix11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Joe on 10/21/2016.
+ */
+public class DowData {
+    private List<Integer> quarter = new ArrayList<>();
+    private List<String> stock = new ArrayList<>();
+    private List<Integer> dayOfYear = new ArrayList<>();
+    private List<Integer> open = new ArrayList<>();
+    private List<Integer> high = new ArrayList<>();
+    private List<Integer> low = new ArrayList<>();
+    private List<Integer> close = new ArrayList<>();
+    private List<Integer> volume = new ArrayList<>();
+    private List<Double> percentChangePrice = new ArrayList<>();
+    private List<Double> percentChangeVolume = new ArrayList<>();
+    private List<Integer> nextWeekOpen = new ArrayList<>();
+    private List<Integer> nextWeekClose = new ArrayList<>();
+    private List<Double> percentChangeNextWeekPrice = new ArrayList<>();
+    private List<Integer> daysToNextDividend = new ArrayList<>();
+    private List<Double> percentReturnNextDividend = new ArrayList<>();
+
+}
