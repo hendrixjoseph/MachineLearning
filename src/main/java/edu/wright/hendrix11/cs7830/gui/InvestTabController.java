e4506d9ca508456c5b2dfef156fb25f85261f253
Joseph Hendrix <hendrixjoseph@aol.com>
2016-12-02 15:51:43 -0500

    Create InvestTabController.java

---
+++ src/main/java/edu/wright/hendrix11/cs7830/gui/InvestTabController.java
@@ -0,0 +1,21 @@
+package edu.wright.hendrix11.cs7830.gui;
+
+import edu.wright.hendrix11.cs7830.DowData;
+import edu.wright.hendrix11.cs7830.Stock;
+import edu.wright.hendrix11.cs7830.StockData;
+import edu.wright.hendrix11.cs7830.machine.LinearStockMachine;
+import edu.wright.hendrix11.cs7830.machine.Machine;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * Created by Joe on 12/1/2016.
+ */
+public class InvestTabController {
+  private Stock stock;
+  
+  public setStock(Stock stock) {
+    this.stock = stock;
+  }
+}
